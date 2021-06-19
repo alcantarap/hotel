87159efb5c5cd375492a2ced8d5f6b90b56fcfc4
daaraneda <dant.araneda@duocuc.cl>
2021-06-18 20:51:42 -0400

    loh quiero mucho

--- Django/core/models.py
+++ Django/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Cliente(models.Models):
+    id_cliente = models.IntegerField(primary_key=True, verbose_name= 'Id Cliente' )
+    nombre = models.CharField(max_length= 100, verbose_name = 'Nombre Cliente' )
+    apellido = models.CharField(max_length= 100, verbose_name = 'Apellido Cliente' )
+    nom_usuario = models.CharField(max_length= 100, verbose_name = 'Nombre Usuario' )
+    num_telefonico = models.IntegerField(max_length= 9, verbose_name = 'Numero de Telefono')
+    correo = models.EmailField(max_length= 100, verbose_name = 'Correo Electronico')
+    contrasena = forms.CharField(label="Password", max_length=50 , widget=forms.PasswordInput, strip=False, verbose_name= 'Contraseña')
+    
+    def __str__(self):
+        return self.nombre
+    
+
