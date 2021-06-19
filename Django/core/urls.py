15bef032dbef64636e6079dc75073b155a669ca3
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2021-06-19 16:51:37 -0500

    Listar y agregar

--- Django/core/urls.py
+++ Django/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import registro
+from .views import add_Cliente, registro, registrados
 
 urlpatterns = [
     path('', registro, name= "registro"),
+    path('registrados/', registrados, name= "registrados"),
+    path('add_cliente/', add_Cliente, name="add_cliente")
 ]
--- Django/core/views.py
+++ Django/core/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render
 from .models import Cliente
 from .forms import ClienteForm
@@ -5,3 +6,20 @@ from .forms import ClienteForm
 
 def registro (request):
     return render(request, 'core/Registro.html')
+
+def registrados (request):
+    cliente = Cliente.objects.all()
+    datos = {
+        'cliente': cliente
+    }
+    return render(request, 'core/lista_Registrados.html', datos)
+
+def add_Cliente (request):
+    datos = {'form': ClienteForm()}
+    if request.method == 'POST':
+        formulario = ClienteForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            datos['mensaje'] = "Guardado Correctamente"
+
+    return render(request, 'core/add_Cliente.html', datos)
