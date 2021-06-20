87ab4aa6c3980c1f07d0aff7c5f89ee39f715584
JoseOrtizMedel <joi.ortiz@duocuc.cl>
2021-06-19 20:47:46 -0500

    HOLA

--- Django/core/views.py
+++ Django/core/views.py
@@ -1,5 +1,5 @@
 from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Cliente
 from .forms import ClienteForm
 # Create your views here.
@@ -14,7 +14,7 @@ def registrados (request):
     }
     return render(request, 'core/lista_Registrados.html', datos)
 
-def add_Cliente (request):
+def add_cliente (request):
     datos = {'form': ClienteForm()}
     if request.method == 'POST':
         formulario = ClienteForm(request.POST)
@@ -23,6 +23,28 @@ def add_Cliente (request):
             datos['mensaje'] = "Guardado Correctamente"
 
     return render(request, 'core/add_Cliente.html', datos)
+
+
+def edit_cliente (request, pk):
+    cliente = Cliente.objects.get(id_cliente=pk)
+
+    if request.method =='POST':
+        formulario_edit = ClienteForm(data=request.POST, instance=cliente)
+        if formulario_edit.is_valid:
+            formulario_edit.save()
+            return redirect(to="registrados")
+    
+    else:
+        datos = {
+            'form':ClienteForm(instance=cliente)
+        }
+        return render(request, 'core/edit_Cliente.html', datos)
+
+
+def delete_cliente(request, pk):
+    cliente = Cliente.objects.get(id_cliente=pk)
+    cliente.delete()
+    return redirect(to="registrados")
 
 
 
